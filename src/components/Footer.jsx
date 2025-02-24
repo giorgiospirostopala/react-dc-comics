e5d10c5fa283e9a8a924b86efe9d428f24f95efc
giorgiospirostopala <gio.topala@gmail.com>
2025-02-24 19:51:48 +0100

    add: """footer"""

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,43 @@
 const Footer = () => {
     return (
-           
+        <footer>
+
+            {/* footerone */}
+            <div className="foto">
+                <div className="container py-5">
+                    <div>
+                        <ul>
+                            <li><h5><strong>TITOLO</strong></h5></li>
+                            <li><p>testo</p></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            {/* footerino */}
+            <div className="container">
+                <div className="d-flex align-items-center justify-content-between">
+                    <div>
+                        <button>
+                            ISCRIVITI
+                        </button>
+                    </div>
+                    <div className="d-flex">
+                        <div>
+                            <h4>SEGUICI</h4>
+                        </div>
+                        <div className="">
+                            <img src="/img/footer-twitter.png" alt="" />
+                            <img src="/img/footer-twitter.png" alt="" />
+                            <img src="/img/footer-twitter.png" alt="" />
+                            <img src="/img/footer-twitter.png" alt="" />
+                            <img src="/img/footer-twitter.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
     )
 };
 
